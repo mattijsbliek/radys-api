928e6ccafecef9e548f37dabb17aa6501ff71a9a
Mattijs Bliek <mattijs@mattijsbliek.com>
2018-10-21 18:22:52 +0200

    Remove unwanted WP Ultimate Recipe Maker content from description meta tag.

--- services/schema/properties/description.js
+++ services/schema/properties/description.js
@@ -1,9 +1,16 @@
 const createQuerySelector = require('utils/createQuerySelector');
 
-module.exports = createQuerySelector('[itemtype$="schema.org/Recipe"] [itemprop="description"], .jetpack-recipe-notes', x => {
-  if (x.tagName === 'META') {
-    return x.getAttribute('content');
-  }
+module.exports = createQuerySelector(
+  '[itemtype$="schema.org/Recipe"] [itemprop="description"], .jetpack-recipe-notes',
+  x => {
+    if (x.tagName === 'META') {
+      return x.content.replace(
+        // Delete invisible content from meta tag
+        /\[wpurp-searchable-recipe\].*\[\/wpurp-searchable-recipe\]/g,
+        '',
+      );
+    }
 
-  return x.innerHTML;
-});
+    return x.innerHTML;
+  },
+);
